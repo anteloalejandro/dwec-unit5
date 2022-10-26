63d556c687b18e71e5130455a8b83a73efd9d474
Alejandro Antelo <alfasant@protonmail.com>
2022-10-26 11:25:08 +0200

    Added activity09

---
+++ js/activity09.js
@@ -0,0 +1,9 @@
+function tribonacci(n) {
+  // Covers base case T0 = 0
+  if (n == 0) return 0
+  // Covers these base cases: T1 = 1 and T2 = 1
+  if (n == 1 || n == 2) return 1
+
+  // T x = T x-1 + T x-2 + T x-3
+  return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3)
+}
